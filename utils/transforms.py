71220f634796a7d2381e4bc3477dea0be0f93741
xqmeng <xiangqiao.meng@connect.polyu.hk>
2023-01-06 22:40:30 +0800

    add gray-scale image entrance

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,17 +1,14 @@
 import torch
 import numpy as np
 from PIL import Image
+from torchvision.transforms import ToPILImage
 
 
 def tensor2PIL(tsr: torch.Tensor):
-    tsr = torch.clamp(tsr.detach().cpu(), min=-1, max=1)
     assert len(tsr.shape) == 4
+    tsr = (torch.clamp(tsr.detach().cpu(), min=-1, max=1).float() + 1.) / 2.
     batch_size = tsr.shape[0]
     ret = []
     for c in range(batch_size):
-        arr = tsr[c].numpy().copy()
-        arr = np.around((arr + 1.0) / 2.0 * 255).astype(int)
-        arr = arr.swapaxes(0, 1)
-        arr = arr.swapaxes(1, 2)
-        ret.append(Image.fromarray(np.uint8(arr)))
+        ret.append(ToPILImage()(tsr[c]))
     return ret
